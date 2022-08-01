d29a78fa088de7924ddb30d56883b89db07ec591
Lenkaa3792 <allanlenkaa@gmail.com>
2022-08-01 20:24:33 +0300

    removing unnecessary spaces in Nav.js

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import{ Navbar,Nav,NavItem,NavbarBrand,Container}from  'reactstrap'
-        
-
+    
 export const Heading = () => {
   return (
       <Navbar color="grey">
