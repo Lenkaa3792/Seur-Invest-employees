70c9fc9b105371721281868c728cffc893710605
Lenkaa3792 <allanlenkaa@gmail.com>
2022-08-02 00:06:15 +0300

    spaces

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import{ Navbar,Nav,NavItem,NavbarBrand,Container}from  'reactstrap'
-
-
-
 export const Heading = () => {
   return (
       <Navbar color="grey">
